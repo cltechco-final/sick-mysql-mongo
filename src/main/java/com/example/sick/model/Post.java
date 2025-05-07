443814cbca3979ae30bfe6eccf11379e5642a7df
Si Young Byun <siyoung.byun@gmail.com>
2025-05-07 13:58:44 +0900

    Update Post.java

--- src/main/java/com/example/sick/model/Post.java
+++ src/main/java/com/example/sick/model/Post.java
@@ -8,5 +8,5 @@ public class Post {
     private String title;
     private String content;
     private String author;
-    private String password; // 취약점: 평문으로 저장되는 비밀번호
+    private String password;
 } 
