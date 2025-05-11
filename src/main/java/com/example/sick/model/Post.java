bfa4a6c426814b4117bf0423faea41f928e641a6
leedy3838 <leedy3838@naver.com>
2025-05-11 15:58:50 +0900

    feat: jpa 옵션 설정

--- src/main/java/com/example/sick/model/Post.java
+++ src/main/java/com/example/sick/model/Post.java
@@ -1,9 +1,15 @@
 package com.example.sick.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
