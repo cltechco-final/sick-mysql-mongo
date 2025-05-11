4bca84ed3a20bf46f39c393344b3bc662afaa425
leedy3838 <leedy3838@naver.com>
2025-05-11 16:10:36 +0900

    feat: table 이름 변경

--- src/main/java/com/example/sick/model/Post.java
+++ src/main/java/com/example/sick/model/Post.java
@@ -3,9 +3,11 @@ package com.example.sick.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Table(name = "posts")
 @Entity
 public class Post {
     @Id
